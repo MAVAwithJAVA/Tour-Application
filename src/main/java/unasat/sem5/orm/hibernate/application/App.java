d75a89ebf41e9c0505d02651fbfdf76b16173008
MAVAwithJAVA <mavajava369@gmail.com>
2023-11-26 21:03:07 -0300

    Functionality for Taking Reports Raw and Text

--- src/main/java/unasat/sem5/orm/hibernate/application/App.java
+++ src/main/java/unasat/sem5/orm/hibernate/application/App.java
@@ -8,32 +8,9 @@ public class App {
     public static void main(String[] args) {
         // Waarom Werkt die Fucking ding nog niet!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-//        EntityManager entityManager = JPAConfiguration.getEntityManager();
-
         LoginService loginService = new LoginService();
         loginService.login();
-
-//        TravelGroupDAO travelGroupDAO = new TravelGroupDAO();
-//        System.out.println(travelGroupDAO.findAllTravelGroups());
-//        System.out.println(travelGroupDAO.findTravelGroupByTraveler("389ht"));
-
-//        TravelSegmentDAO travelSegmentDAO = new TravelSegmentDAO();
-//        System.out.println(travelSegmentDAO.findAllTravelSegments());
-//        System.out.println(travelSegmentDAO.retrieveTravelSegmentByDestinationFrequency());
-//
-//        TravelPackageDAO travelPackageDAO = new TravelPackageDAO();
-//        RawReport rawReport = new RawReport(travelPackageDAO.retrieveLastTravelPackageReport());
-//        rawReport.displayReport();
-
-
         JPAConfiguration.shutdown();
-
-
-
-
-
-
-
 
     }
 }
--- src/main/java/unasat/sem5/orm/hibernate/dao/TravelSegmentDAO.java
+++ src/main/java/unasat/sem5/orm/hibernate/dao/TravelSegmentDAO.java
@@ -4,17 +4,12 @@ import unasat.sem5.orm.hibernate.config.JPAConfiguration;
 import unasat.sem5.orm.hibernate.entities.TravelSegment;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class TravelSegmentDAO {
 
     private EntityManager entityManager = JPAConfiguration.getEntityManager();
-
-//    public TravelSegmentDAO(EntityManager entityManager) {
-//        this.entityManager = entityManager;
-//    }
 
     public List<TravelSegment> retrieveTravelSegmentByDestinationFrequency() {
         List<TravelSegment> travelSegmentList;
@@ -26,20 +21,10 @@ public class TravelSegmentDAO {
         return travelSegmentList;
     }
 
-    public List<TravelSegment> findAllTravelSegments() {
-        entityManager.getTransaction().begin();
-        String jpql = "select t from TravelSegment t";
-        Query query = entityManager.createQuery(jpql);
-        List<TravelSegment> travelSegmentList = query.getResultList();
-        entityManager.getTransaction().commit();
-        return travelSegmentList;
-    }
-
     public TravelSegment insertTravelSegment(TravelSegment travelSegment) {
         entityManager.getTransaction().begin();
         entityManager.persist(travelSegment);
         entityManager.getTransaction().commit();
-//        System.out.println("Travel segment has been inserted");
         return travelSegment;
     }
 
